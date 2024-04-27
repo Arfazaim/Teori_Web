36ca05e8fa62d9126551eb3b7b2aeb0376611821
Arfa Zaim Al Murtadlo <arfazaim0202@gmail.com>
2024-04-27 20:02:22 +0700

    teori

---
+++ Modul_4/get.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nama:</h1> <?php echo $_GET ['nama']; ?>
+    <h1>Alamat:</h1> <?php echo $_GET ['alamat']; ?>
+</body>
+</html>
